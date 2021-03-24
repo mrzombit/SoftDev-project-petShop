d85091c4755902b95f99d6e2e5f14ba74bc8d13e
mrzombit <54615665+mrzombit@users.noreply.github.com>
2021-03-25 06:54:20 +0700

    Add files via upload

--- src/components/page/homePage/slider.js
+++ src/components/page/homePage/slider.js
@@ -1,41 +1,44 @@
-import React from "react";
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
-"mdbreact";
+
+import React,{useState, useEffect} from 'react'
+
 
 import News_dog_src from './img/news_dog.png'
 import News_dog2_src from './img/news_dog2.jpg'
-import './home.css';
+import Slider_icon_src from './img/slider_icon.png'
+import './home2.css';
 
 const Slider = () => {
+  const [newsPic, setNewsPic] = useState(News_dog_src)
+  const [sliderIcon, setSliderIcon] = useState('slider-icon')
+  const [sliderIcon2, setSliderIcon2] = useState('slider-icon2')
+
+  const setSliderIcons =( pic ) =>{
+
+    if ( pic == 1){
+      setNewsPic(News_dog_src)
+      setSliderIcon('slider-icon')
+      setSliderIcon2('slider-icon2')
+    }
+    else {
+      setNewsPic(News_dog2_src)
+      setSliderIcon('slider-icon2')
+      setSliderIcon2('slider-icon')
+    }
+  }
+
+
   return (
-    <MDBContainer>
-      <MDBCarousel
-      activeItem={1}
-      length={2}
-      showControls={true}
-      showIndicators={true}
-      className="z-depth-1"
-    >
-      <MDBCarouselInner>
-        <MDBCarouselItem itemId="1">
-          <MDBView>
-            <img
-              className="news-img"
-              src={News_dog_src}
-            />
-          </MDBView>
-        </MDBCarouselItem>
-        <MDBCarouselItem itemId="2">
-          <MDBView>
-            <img
-              className="news-img"
-              src={News_dog2_src}
-            />
-          </MDBView>
-        </MDBCarouselItem>
-      </MDBCarouselInner>
-    </MDBCarousel>
-    </MDBContainer>
+    <div className='center-pad'>
+      <img className ='news-img' src={newsPic}/>
+      <div className='slider-font-pane'>
+        <div className = 'slider-pane'>
+          <img className= {sliderIcon} src = {Slider_icon_src} onClick={ () =>setSliderIcons(1)}/>
+          <img className= {sliderIcon2} src = {Slider_icon_src} onClick={ () =>setSliderIcons(2)}/>
+        </div>
+      </div>
+      
+      
+    </div>
   );
 }
 
